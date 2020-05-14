9d3a4b9a4ea7f4e975956d7bd99fcb7a8ee50edb
Cory Fowler <cfowler@microsoft.com>
2020-05-14 10:37:06 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿namespace protocol_change
+{
+    using System;
+    using System.Net;
+
+    class Program
+    {
+        protected static bool TestUrl(string url)
+        {
+            var wr = (HttpWebRequest)WebRequest.Create(url);
+
+            try
+            {
+                var hwr = (HttpWebResponse)wr.GetResponse();
+                return (hwr.StatusCode == HttpStatusCode.OK);
+            }
+            catch (WebException wex)
+            {
+                var statusCode = ((HttpWebResponse)wex.Response).StatusCode;
+
+                switch (statusCode)
+                {
+                    case HttpStatusCode.Unauthorized:
+
+                    break;
+                    case HttpStatusCode.InternalServerError:
+                    break;
+                    default:
+
+                    break;
+                }
+                return false;
+            }
+        }
+
+        protected static string ChangeUriScheme(UriBuilder uri)
+        {
+            uri.Scheme = Uri.UriSchemeHttps;
+            uri.Port = -1;
+
+            return uri.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+
+                try
+                {
+                    UriBuilder url = new UriBuilder(item);
+                    if (url.Scheme != Uri.UriSchemeHttp) continue;
+
+                    string updatedUri = ChangeUriScheme(url);
+
+                    Console.WriteLine("URL " + updatedUri + " Passes: " + (TestUrl(updatedUri) ? "Yes" : "No"));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    continue;
+                }
+            }
+        }
+    }
+}
